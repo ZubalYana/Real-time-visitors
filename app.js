e837ecc7bc5114d7aa7f81316f5c251f66b6a5af
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-03 15:30:34 +0300

    run server

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const express = require('express')
+const http = require('http').createServer(app);
+const io = require('socket.io')(http)
+const PORT = 3000
+const path = require('path')
+const mongoose = require('mongoose');
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname, 'public', 'index.html')
+})
+http.listen(PORT, ()=>{
+    console.log(`Server works on PORT: ${PORT}`)
+})
