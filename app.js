d9321473ba13f40a7b3f70d8ef385ea83b7798b4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-03 16:24:15 +0300

    connect mongoose

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 const TOKEN = '7121504275:AAHPive5eXJbB8RssVnYWZeBgloZZ7nXGvs';
 const chatId = '1132590035'
 const bot = new TelegramBot(TOKEN, {polling: true})
-
+mongoose.connect(`mongodb+srv://zubalana0:pscc865qqDLqzpv3@cluster0.n2nxzri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+    console.log(`Connected to mongo DB`)
+})
 io.on('connection', (socket)=>{
     console.log(`New user connected`)
     bot.sendMessage(chatId, `New user connected`)
