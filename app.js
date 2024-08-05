538f021f8bf8a7179f0db7e2015d2350b99f86c0
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-05 13:18:58 +0300

    try to create a basic users amount counting function

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 const express = require('express');
-const http = require('http')
-const socketIo = require('socket.io')
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server)
-const PORT = 3000
-const path = require('path')
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = 3000;
+const path = require('path');
 const mongoose = require('mongoose');
 const TelegramBot = require('node-telegram-bot-api');
-app.use(express.static(path.join(__dirname, 'public')))
-const TOKEN = '7121504275:AAHPive5eXJbB8RssVnYWZeBgloZZ7nXGvs';
-const chatId = '1132590035'
-const bot = new TelegramBot(TOKEN, {polling: true})
+app.use(express.static(path.join(__dirname, 'public')));
+
+const TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
+const chatId = 'YOUR_CHAT_ID';
+const bot = new TelegramBot(TOKEN, { polling: true });
+
 mongoose.connect(`mongodb+srv://zubalana0:pscc865qqDLqzpv3@cluster0.n2nxzri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-.then(()=>{
-    console.log(`Connected to mongo DB`)
-})
+  .then(() => {
+    console.log(`Connected to MongoDB`);
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+  });
 
-io.on('connection', (socket)=>{
-    console.log(`New user connected`)
-    bot.sendMessage(chatId, `New user connected`)
-    socket.on('disconnect', ()=>{
-        console.log(`User disconnected`)
-    })
-})
+const userCountSchema = new mongoose.Schema({
+  usersCount: { type: Number, default: 0 }
+});
 
+const UserCount = mongoose.model('UserCount', userCountSchema);
 
+async function initializeUserCount() {
+  let countDoc = await UserCount.findOne();
+  if (!countDoc) {
+    countDoc = new UserCount({ usersCount: 0 });
+    await countDoc.save();
+  }
+  return countDoc;
+}
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname, 'public', 'index.html')
-})
+io.on('connection', async (socket) => {
+  console.log(`New user connected`);
+  bot.sendMessage(chatId, `New user connected`);
 
-server.listen(PORT, ()=>{
-    console.log(`Server works on PORT: ${PORT}`)
-})
+  try {
+    const countDoc = await initializeUserCount();
+    countDoc.usersCount += 1;
+    await countDoc.save();
+
+    const updatedCount = await UserCount.findOne();
+    console.log(updatedCount);
+
+    socket.on('disconnect', async () => {
+      console.log(`User disconnected`);
+      bot.sendMessage(chatId, `User disconnected`);
+
+      countDoc.usersCount -= 1;
+      await countDoc.save();
+
+      const updatedCountAfterDisconnect = await UserCount.findOne();
+      console.log(updatedCountAfterDisconnect);
+    });
+  } catch (err) {
+    console.error(`Error updating user count: ${err.message}`);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
