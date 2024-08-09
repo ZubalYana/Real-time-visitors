a247e8b104a1571672ce6755ecc06a3d5e1040b5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-09 07:29:17 +0300

    finish adjusting user count

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const TelegramBot = require('node-telegram-bot-api');
 app.use(express.static(path.join(__dirname, 'public')));
-
 const TOKEN = '7121504275:AAHPive5eXJbB8RssVnYWZeBgloZZ7nXGvs';
 const chatId = '1132590035';
 const bot = new TelegramBot(TOKEN, { polling: true });
@@ -28,7 +27,7 @@ const userCountSchema = new mongoose.Schema({
 
 const UserCount = mongoose.model('UserCount', userCountSchema);
 
-async function initializeUserCount() {
+async function countUserCount() {
   let countDoc = await UserCount.findOne();
   if (!countDoc) {
     countDoc = new UserCount({ usersCount: 0 });
@@ -40,22 +39,17 @@ async function initializeUserCount() {
 io.on('connection', async (socket) => {
   console.log(`New user connected`);
   bot.sendMessage(chatId, `New user connected`);
-
   try {
-    const countDoc = await initializeUserCount();
+    const countDoc = await countUserCount();
     countDoc.usersCount += 1;
     await countDoc.save();
-
     const updatedCount = await UserCount.findOne();
     console.log(updatedCount);
-
     socket.on('disconnect', async () => {
       console.log(`User disconnected`);
       bot.sendMessage(chatId, `User disconnected`);
-
       countDoc.usersCount -= 1;
       await countDoc.save();
-
       const updatedCountAfterDisconnect = await UserCount.findOne();
       console.log(updatedCountAfterDisconnect);
     });
